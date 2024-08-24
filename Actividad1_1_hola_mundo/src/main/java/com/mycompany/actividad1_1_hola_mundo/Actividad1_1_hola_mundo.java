7fd81444fd02e7f9a210c5487fb446dbf2f60424
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-08-23 21:12:33 -0600

    Add files via upload

---
+++ Actividad1_1_hola_mundo/src/main/java/com/mycompany/actividad1_1_hola_mundo/Actividad1_1_hola_mundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad1_1_hola_mundo;
+
+/**
+ *
+ * @author 52452
+ */
+public class Actividad1_1_hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
