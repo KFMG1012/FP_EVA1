a88037699503e5bbdd4822a09816cf5cb1028308
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-02 23:45:54 -0600

    Add files via upload

---
+++ EVA1_4_TVM/src/main/java/com/mycompany/eva1_4_tvm/EVA1_4_TVM.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_tvm;
+
+/**
+ *
+ * @author 52452
+ */
+public class EVA1_4_TVM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                String Marca;
+        String Modelo;
+        String Tamaño;
+        double precio;
+        Marca = "samnsung";
+        Modelo="samtv12";
+        Tamaño="54.5pulgadas";
+        precio=50000;
+        System.out.println(Marca);
+          System.out.println(Modelo);
+            System.out.println(Tamaño);
+                        System.out.println(precio);
+    }
+}
