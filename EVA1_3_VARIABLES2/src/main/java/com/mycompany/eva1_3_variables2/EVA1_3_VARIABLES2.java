d83e8d434392428de7c8fa6a0754f1b0bbc5005c
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-02 23:45:04 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/main/java/com/mycompany/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_variables2;
+
+/**
+ *
+ * @author 52452
+ */
+public class EVA1_3_VARIABLES2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+           String marca;
+        String modelo;
+        int year;// la ñ en otros lenguajes no es aceptada
+        double precio;
+        //Asignacion
+        marca="nissan";
+        modelo="GTR";
+        year=2008;
+        precio=1000000.1; 
+        System.out.println(marca);
+         System.out.println(modelo);
+          System.out.println(year);
+           System.out.println(precio);
+    }
+}
