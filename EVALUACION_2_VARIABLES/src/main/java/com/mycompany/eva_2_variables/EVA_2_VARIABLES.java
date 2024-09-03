2c726d9c2230f3b21137f3dccf0d81074cbd855a
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-02 23:43:13 -0600

    Add files via upload

---
+++ EVALUACION_2_VARIABLES/src/main/java/com/mycompany/eva_2_variables/EVA_2_VARIABLES.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_2_variables;
+
+
+public class EVA_2_VARIABLES {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");//CADENA DE TEXTO
+        System.out.println("100");//ENTERO
+        System.out.println("c");//CARACTER
+        System.out.println("tru");//VALOR LOGICO (BOOLEANOS)
+        System.out.println("false");//
+        System.out.println("3.1416");//NUMERO CON PUNTO FLOTANTE
+        //;--FIN DE INSTRUCCIONES(NO OLVIDE PONERLO)
+        //ENTERO
+        //int
+        //long
+        //short
+        
+        //CADENA DE TEXTO
+        //string
+        
+        //CARACTERES
+        //char
+        
+        //VALORES LOGICOS
+        //boolean
+        
+        //DECLARACIONES DE VARIABLES
+        //SINTAXIS PARA DECLARAR VARIABLES(REGLAS)
+        //TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE;
+        String nombre;
+        String apellido;
+        double promedio;//asignacion: estoycolocando el valor 18
+                //dentro de la variable edad
+        int edad;
+        edad=18;
+  
+                nombre = "nombre";
+                apellido ="Mejia Gutierrez";
+                promedio=1.0;
+                        System.out.println(edad);
+                         System.out.println(nombre);
+                          System.out.println(apellido);
+                           System.out.println(promedio);
+    }
+}
