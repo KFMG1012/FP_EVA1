d80f65ae332182f202bd1867f2d3bae27d0af598
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-03 23:17:50 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES/src/main/java/com/mycompany/eva1_9_operaciones/EVA1_9_OPERACIONES.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_operaciones;
+
+/**
+ *
+ * @author 52452
+ */
+public class EVA1_9_OPERACIONES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //operaciones aritmeticas
+        //suma y resta
+        int x=10; //declaracion de asignacion
+        int y=5;
+        //si necesitan muchas variables:
+        int suma,resta,multiplicacion,division,potencia,raiz;
+        //suma y resta
+        suma=x+y;
+         System.out.println("la suma de x+y:");
+        System.out.println(suma);
+        resta=x-y;
+          System.out.println("la resat de x-y:");
+        System.out.println(resta);
+        //multiplicacion
+        multiplicacion =x*y;
+         System.out.println("la multiplicacion de x(10)*y(5):");
+        System.out.println(multiplicacion);
+        division =x/y;
+          System.out.println("la division de x(5)/y(2):");
+        System.out.println(division);
+        x=5;
+        y=2;
+           System.out.println("la division de x(5)/y(2):");
+        System.out.println(division);
+        //Divisiones con punto decimal flotante
+        double val1=5;
+                double val2=2;
+                        double resu;
+                        resu=val1/val2;
+                                System.out.println("el resultado de la divion es:");
+                                 System.out.println(resu);
+        
+        resu=x/y;
+    }
+}
