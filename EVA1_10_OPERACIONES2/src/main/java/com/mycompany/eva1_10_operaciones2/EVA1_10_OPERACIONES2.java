f8d555f37c960cc08a6c0c567958d3f402f37f62
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-03 23:18:47 -0600

    Add files via upload

---
+++ EVA1_10_OPERACIONES2/src/main/java/com/mycompany/eva1_10_operaciones2/EVA1_10_OPERACIONES2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_operaciones2;
+
+/**
+ *
+ * @author 52452
+ */
+public class EVA1_10_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Concatenacion cadenas
+        //operador de concatenacion +
+        //mismo simbolo con diferentes usos
+        //sobrecarga de operadores
+        String nombre="ruben";
+         String apellido="Hernandez";
+           
+         String NomCom;
+         NomCom= nombre + " " + apellido;
+         System.out.println(NomCom);
+         System.out.println("nombre del usuario " +nombre);
+        System.out.println("apellido del usuario " +apellido);
+        System.out.println("nombre completo del usuario " +NomCom);
+    }
+}
