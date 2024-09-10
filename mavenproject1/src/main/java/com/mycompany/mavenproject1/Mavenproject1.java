9a905bbe3da97ea166d82f5c8763d1e380662b03
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-10 11:25:50 -0600

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 52452
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int edad;
+        Scanner captu;
+        captu= new Scanner(System.in);
+        
+        System.out.println("dame la edad");
+        edad=captu.nextInt();
+        System.out.println("la edad es "+edad);
+      if(edad>=18) 
+      {System.out.println("es mayor de edad");
+    
+      }
+      else{
+    System.out.println("es menor de edad");
+    
+    }
+      
+      
+      
+      
+      
+    }
+   
+                                 
+}
