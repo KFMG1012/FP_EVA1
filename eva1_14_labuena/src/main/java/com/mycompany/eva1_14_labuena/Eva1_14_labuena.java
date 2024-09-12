bd2a1b6a1999833bb2b3319d57e36c30609d520f
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-11 23:31:37 -0600

    Add files via upload

---
+++ eva1_14_labuena/src/main/java/com/mycompany/eva1_14_labuena/Eva1_14_labuena.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_14_labuena;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva1_14_labuena {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+              int cali;
+        Scanner captu;
+        captu=new Scanner(System.in);
+        System.out.println("dame la cali");
+        cali=captu.nextInt();
+        if(cali>=6){
+        System.out.println("Aprobaste");
+        
+        } else
+       
+        {
+                System.out.println("reprobaste");
+                
+                }
+    }
+}
