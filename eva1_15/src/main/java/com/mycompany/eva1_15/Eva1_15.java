ec8532aa3da624cfd06ef0cb8b0a35295e44c06b
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-09-11 23:32:12 -0600

    Add files via upload

---
+++ eva1_15/src/main/java/com/mycompany/eva1_15/Eva1_15.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_15;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva1_15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        double valor=Math.random();
+        if(valor<=0.5)  {
+            System.out.println("aguila");
+            }
+        else{
+        
+        
+        System.out.println("sello");
+        }
+    }
+    
+}
